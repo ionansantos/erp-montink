fb635effbafee226717e32e74ccd3494faf88896
ionan santos <ionan564@gmail.com>
2025-09-24 02:17:34 -0300

    create controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,8 +1,19 @@
 <?php
 
 namespace App\Http\Controllers;
+use illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Request;
+
 
 abstract class Controller
 {
-    //
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    function __construct(Request $request)
+    {
+
+    }
 }
